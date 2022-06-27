b9a314afc62b6385def616c8a09f16576b4ef601
SergeyRushev <rushev84@yandex.ru>
2022-06-27 10:33:27 +0300

    feat: dropzone-element

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        Index
+    <div class="w-25">
+        <div ref="dropzone" class="btn d-block p-5 bg-dark text-center text-light">Upload</div>
     </div>
 </template>
 
 <script>
+    import Dropzone from 'dropzone'
     export default {
-        name: "Index"
+        name: "Index",
+
+        data() {
+            return {
+                dropzone: null
+            }
+        },
+
+        mounted() {
+            this.dropzone = new Dropzone(this.$refs.dropzone, {
+                url: "sometext"
+            })
+        }
     }
 </script>
 
